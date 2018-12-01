b3947e6960007aea0e18a73f0a8f20b61c1afb5d
PhelanM <phelanmccall@yahoo.com>
2018-12-01 00:55:28 -0800

    concert-this up and running

--- liri.js
+++ liri.js
@@ -1,12 +1,24 @@
 require("dotenv").config();
 var request = require('request');
 var keys = require("./keys.js");
+var moment = require("moment");
+moment().format();
 
 var concert = function (artist) {
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-   request.get(queryURL).on("response", function(response){
-       console.log(JSON.stringify(response, null, 2))
-   })
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bandsintown.id + "&date=upcoming";
+   request(queryURL, function(error, response, body){
+    
+    var obj = JSON.parse(body);
+    
+    if(obj[0]){
+    var ven = obj[0].venue;
+    console.log(`VENUE NAME: \n${ven.name}`);
+    console.log(`LOCATION: \n${ven.city}, ${ven.region.length != "" ? ven.region : ven.country}`);
+    console.log(`DATE: \n${moment(obj[0].datetime).format("MMMM DD YYYY")}`);
+    }else{
+        console.log("No concert found. :(")
+    }
+   });
 }
 
 var spotify = function(song){
@@ -17,15 +29,21 @@ var movie = function(movie){
 
 }
 
-var parseTxt(txt){
+var parseTxt = function(txt){
 
 }
 
-if (process.argv[2]) {
+if (process.argv.length > 2) {
     switch (process.argv[2].toLowerCase()) {
         case "concert-this":
-            if (process.argv[3]) {
-                concert(process.argv[3].toLowerCase());
+            if (process.argv.length > 3) {
+                var artist = [];
+                for(let i = 3; i < process.argv.length; i++){
+                    artist.push(process.argv[i]);
+                }
+                concert(artist.join("%20").toLowerCase());
+            }else{
+                console.log("Please enter an artist.");
             }
             break;
         case "spotify-this-song":
@@ -38,6 +56,8 @@ if (process.argv[2]) {
             console.log("ERROROROROOROROROR");
             break;
     }
+}else{
+    console.log("Give me some inputs!;")
 }
 
 
